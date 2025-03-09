61d71e4131f7bac31a38fad40a0ef856a7f5f8f0
reetou <fillmasterson@gmail.com>
2025-03-09 21:01:26 +0100

    Add meta to initiate user job

--- lib/mailseek/gmail.ex
+++ lib/mailseek/gmail.ex
@@ -2,10 +2,16 @@ defmodule Mailseek.Gmail do
   alias Mailseek.Jobs.InitiateGmailUser
 
   def initiate_user(user_id) do
-    InitiateGmailUser.new(%{
-      "provider" => "gmail",
-      "user_id" => user_id
-    })
+    InitiateGmailUser.new(
+      %{
+        "provider" => "gmail",
+        "user_id" => user_id
+      },
+      meta: %{
+        "type" => "initiate_gmail_user",
+        "user_id" => user_id
+      }
+    )
     |> Oban.insert!()
   end
 end
